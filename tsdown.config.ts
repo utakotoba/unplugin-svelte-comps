a522591d6b9dcc7d454f1d5d3896331cd7053643
Ling Yu <echo.aimless@gmail.com>
2026-05-15 13:59:44 +0800

    chore: switch to ts native preview

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: 'src/index.ts',
   format: ['es', 'cjs'],
-  dts: true,
+  dts: { tsgo: true },
   tsconfig: './tsconfig.json',
 })
